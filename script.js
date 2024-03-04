664017085769fbbafbbe2e6b77fce6c426969ee4
Stefani Dal Puppo <stefanipuppo@hotmail.com>
2024-03-04 18:03:30 -0300

    adicionei mais artistas

--- script.js
+++ script.js
@@ -1,24 +1,40 @@
+let artistas = ["gal", "liniker", "joao gilberto"];
+let figura = {"gal":"gal.jpg", "liniker":"liniker.jpg", "joao gilberto":"joaogil.jpg"};
+
+let indiceAtual = 0;
+
+
 function verificarResposta() {
     let respostaUsuario = document.getElementById("resposta").value.toLowerCase();
-
-    let respostaCorreta = "gal costa"
-
     let resultado = document.getElementById("resultado");
 
-    if (respostaUsuario === respostaCorreta){
-        resultado.textContent = "Parabens você acertou!";
+    if (respostaUsuario === artistas[indiceAtual] && figura[artistas[indiceAtual]])
+    {
+        resultado.textContent = "Parabéns, você acertou!";
         resultado.style.color = "green";
-        }
 
-        else {
-            resultado.textContent = "Resposta incorreta. Tente novamente."
-            resultado.style.color = "red";
-        }
-    
+        document.getElementById("verificar").style.display = "none";
+        document.getElementById("proximo").style.display = "inline";
+    } 
+    else {
+        resultado.textContent = "Resposta incorreta. Tente novamente.";
+        resultado.style.color = "red";
+    }
+}
 
+function proximaPergunta() {
+    indiceAtual++;
+    if (indiceAtual < artistas.length) {
+        let imagemArtista = document.querySelector(".quiz img");
+        
 
+        imagemArtista.src = figura[artistas[indiceAtual]];
 
-
-
-
+        document.getElementById("resposta").value = "";
+        document.getElementById("resultado").textContent = "";
+        document.getElementById("verificar").style.display = "inline";
+        document.getElementById("proximo").style.display = "none";
+    } else {
+        alert("Parabéns, você terminou o quiz!");
+    }
 }
